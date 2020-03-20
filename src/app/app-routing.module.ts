e3203f4b80f4986506c77b394899bfb47af1dce5
Michael Nickel <micha_1997@web.de>
2020-03-20 10:21:53 +0100

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { HelpComponent } from './help/help.component';
-import { ExamComponent } from './exam/exam.component';
-import { ExerciseComponent } from './exercise/exercise.component';
-import { UploadComponent } from './upload/upload.component';
-import { GeneratePdfComponent } from './generate-pdf/generate-pdf.component';
-import { ExerciseDetailsComponent } from './exercise/exercise-details/exercise-details.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {HelpComponent} from './help/help.component';
+import {ExamComponent} from './exam/exam.component';
+import {ExerciseComponent} from './exercise/exercise.component';
+import {UploadComponent} from './upload/upload.component';
+import {GeneratePdfComponent} from './generate-pdf/generate-pdf.component';
+import {ExerciseDetailsComponent} from './exercise/exercise-details/exercise-details.component';
+import {ExaminerComponent} from './examiner/examiner.component';
 
 
 const routes: Routes = [
-  { path: '', component: MainComponent },
-  { path: 'help', component: HelpComponent },
-  { path: 'exam', component: ExamComponent },
-  { path: 'exercise', component: ExerciseComponent },
-  { path: 'exercise/:id', component: ExerciseDetailsComponent },
-  { path: 'upload', component: UploadComponent },
-  { path: 'pdf', component: GeneratePdfComponent }
+  {path: '', component: MainComponent},
+  {path: 'help', component: HelpComponent},
+  {path: 'exam', component: ExamComponent},
+  {path: 'exercise', component: ExerciseComponent},
+  {path: 'exercise/:id', component: ExerciseDetailsComponent},
+  {path: 'upload', component: UploadComponent},
+  {path: 'pdf', component: GeneratePdfComponent},
+  {path: 'examiner', component: ExaminerComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
